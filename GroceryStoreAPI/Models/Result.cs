3f3d782f877426e85808eeb8b6646277cd5c126f
Scott Simontis <ssimontis@gmail.com>
2021-06-22 12:06:49 -0400

    Added Result class
    * Added generic Result model in preparation for addition of CQRS requests and responses

---
+++ GroceryStoreAPI/Models/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace GroceryStoreAPI.Models
+{
+    public class Result<T> where T : class
+    {
+        public T Data { get; }
+        
+        public string ErrorMessage { get; }
+        
+        public HttpStatusCode StatusCode { get; }
+
+        public bool IsSuccess => Data != default(T);
+
+        public Result(T data, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            Data = data;
+            StatusCode = statusCode;
+        }
+
+        public Result(string errorMessage, HttpStatusCode statusCode)
+        {
+            Data = default;
+            ErrorMessage = errorMessage;
+            StatusCode = statusCode;
+        }
+        
+    }
+}
