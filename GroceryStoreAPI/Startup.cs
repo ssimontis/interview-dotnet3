56461e69f8c6900b54cccadcd2da271f292be9f3
Scott Simontis <ssimontis@gmail.com>
2021-06-23 10:13:52 -0400

    Removing Warning
    * Removing obsolete IHostingEnvironment warning by implementing Microsoft's recommended replacement

--- GroceryStoreAPI/Startup.cs
+++ GroceryStoreAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace GroceryStoreAPI
@@ -55,7 +56,7 @@ namespace GroceryStoreAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
